e4137aced4eef74bed5687eda421944f6ef7b109
Y2ktorrez <andrestorrezvaca@gmail.com>
2025-04-18 10:10:00 -0400

    frontend login and create account

--- codeflex-parcial1_frontend/src/app/layout.tsx
+++ codeflex-parcial1_frontend/src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { ClerkProvider } from "@clerk/nextjs";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,12 +25,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <Navbar />
+
+          {/* Background gradient and grid */}
+          <div className="fixed inset-0 -z-1">
+            <div className="absolute inset-0 bg-gradient-to-b from-background via-background to-background"></div>
+            <div className="absolute inset-0 bg-[linear-gradient(var(--cyber-grid-color)_1px,transparent_1px),linear-gradient(90deg,var(--cyber-grid-color)_1px,transparent_1px)] bg-[size:20px_20px]"></div>
+          </div>
+
+          <main className="pt-24 flex-grow">
+            {children}
+          </main>
+          
+          <Footer />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
